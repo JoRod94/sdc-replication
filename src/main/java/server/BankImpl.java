b22a52d13dc24758a7268f1967d3d1f7b0e8af26
Fernando Mendes <devfrmendes@gmail.com>
2016-04-16 12:41:32 +0100

    Updates iml file

--- src/main/java/server/BankImpl.java
+++ src/main/java/server/BankImpl.java
@@ -1,56 +1,8 @@
 package server;
 
-import bank.Bank;
-
-import java.io.Serializable;
-import java.util.Map;
-import java.util.TreeMap;
-
 /**
  * Created by joaorodrigues on 14 Apr 16.
  */
-public class BankImpl implements Bank, Serializable {
-    private TreeMap<String, Integer> accounts;
-    private int id;
-
-    public BankImpl() {
-        accounts = new TreeMap<>();
-        id = 0;
-    }
-
-    public BankImpl(BankImpl b) {
-        System.out.println("RECOVERING STATE...");
-        accounts = b.accounts;
-        id = b.id;
-        for(Map.Entry<String, Integer> p : accounts.entrySet())
-            System.out.println("- " + p.getKey() + " :: " + p.getValue());
-    }
-
-    @Override
-    public String create(int amount) {
-        String accountId = Integer.toString(++id);
-        accounts.put(accountId, amount);
-        return accountId;
-    }
-
-    @Override
-    public Integer balance(String account) {
-        return accounts.get(account);
-    }
-
-    @Override
-    public boolean movement(String account, int amount) {
-        Integer balance = accounts.get(account);
-
-        if(balance == null || (amount < 0 && amount + balance < 0))
-            return false;
-
-        accounts.put(account, amount + balance);
-        return true;
-    }
-
-    @Override
-    public boolean transfer(String origin, String destination, int amount) {
-        return movement(origin, amount) && movement(destination, amount);
-    }
+public class BankImpl {
+    private Integer balance;
 }
