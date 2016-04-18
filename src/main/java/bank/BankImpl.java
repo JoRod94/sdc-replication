2c15e488ea8320d06601b61575e53e0c790b6c24
Fernando Mendes <devfrmendes@gmail.com>
2016-04-18 11:15:33 +0100

    Picks BankImpl from jr-data-access

---
+++ src/main/java/bank/BankImpl.java
@@ -0,0 +1,68 @@
+package bank;
+
+import data.DataAccess;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * Created by joaorodrigues on 14 Apr 16.
+ */
+public class BankImpl implements Bank, Serializable {
+    private TreeMap<String, Integer> accounts;
+    private int id;
+
+    public BankImpl() {
+        accounts = new TreeMap<>();
+        id = 0;
+    }
+
+    public BankImpl(BankImpl b) {
+        System.out.println("RECOVERING STATE...");
+        accounts = b.accounts;
+        id = b.id;
+        for(Map.Entry<String, Integer> p : accounts.entrySet())
+            System.out.println("- " + p.getKey() + " :: " + p.getValue());
+    }
+
+
+    //Used when recovering
+    public BankImpl(DataAccess transactionList, Set<BankTransaction> transactions) {
+        //Aplicar todas as transaçoes, estas sao a partir do ID mais recente.
+    }
+
+    //Used when not recovering
+    public BankImpl(DataAccess transactionList) {
+
+    }
+
+    @Override
+    public String create(int amount) {
+        String accountId = Integer.toString(++id);
+        accounts.put(accountId, amount);
+        return accountId;
+    }
+
+    @Override
+    public Integer balance(String account) {
+        return accounts.get(account);
+    }
+
+    @Override
+    public boolean movement(String account, int amount) {
+        Integer balance = accounts.get(account);
+
+        if(balance == null || (amount < 0 && amount + balance < 0))
+            return false;
+
+        accounts.put(account, amount + balance);
+        return true;
+    }
+
+    @Override
+    public boolean transfer(String origin, String destination, int amount) {
+        return movement(origin, amount) && movement(destination, amount);
+    }
+}
